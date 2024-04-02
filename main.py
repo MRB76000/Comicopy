af661dd5000548e1d49e2b631903f4f0aac1d5d8
MRB76000 <mb840705@gmail.com>
2024-04-01 22:28:16 -0400

    made json file to store comic keywords and links, added python foundation

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-#comment
-print("this is a new feature")
-print("here is the second feature, that hopefully merges the branches")
-print("pull request")
+#fresh start
+from bs4 import BeautifulSoup
+import urllib.request
+import shutil
+import os
+import time
+import json
+
+
+
+
+intro = str(input("Welcome to Comicopy!! What would you like to do? \n \nd: download comics\np: post comic title to database\n\n:"))
+
+if intro == "d":
+    #comic downloader
+    None
+elif intro == "p":
+    #comic uploader
+    seriesLink = input("Please paste the link of the comic series/an issue of it:\n")
+    keyword = input("\nWhat would you like they keyword for this Series to be?\n")
+    fake = {
+        keyword: seriesLink
+    }
+    database = open("db.json", 'w').write("\n" + json.dumps(fake))
+    
+
+
+
