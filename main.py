d708dd4843f9cbde6df3efb4db80c91fb9b29c1a
MRB76000 <mb840705@gmail.com>
2024-04-05 22:27:46 -0400

    So many changes. Implemented database features, deletion features, ui changes, functions, error messages, and moved from json to sqlite

--- main.py
+++ main.py
@@ -1,34 +1,78 @@
-#fresh start
 from bs4 import BeautifulSoup
 import urllib.request
 import shutil
 import os
 import time
-import json
+import sqlite3
 
-# def downloader():
+def home():
+    return "lol"
+
+#establish db
+con = sqlite3.connect("library.db")
+cursor = con.cursor()
+try:
+    cursor.execute("CREATE TABLE comic(key, link, lastIssue)")
+    con.commit()
+
+except:
+    con.commit()
+
+def error(message, current):
+    ans = input(message)
+    if ans.lower() == 'r':
+        current()
+    elif ans.lower() == 'h':
+        home()
+    elif ans.lower() == 'x':
+        None
+    else:
+        error("\nYou did not provide a valid input, please press r, h or x\n\n", current=current)
+
+
+
     
 
 
+def uploader():
+    seriesLink = input("\nPlease paste the link of the comic series:\n")
+    keyword = input("\nWhat would you like the keyword for this Series to be?\n")
+    if cursor.execute("SELECT * FROM comic WHERE key = \'" + keyword + "\' OR link = \'" + seriesLink + "\'").fetchall() != []:
+        error("\nIt seems like that keyword or link is already in your library, maybe try again? \nx: Exit Program\nr: Restart Task\nh: Go Home\n\n", uploader)
+    else:
+        better = str("INSERT INTO comic VALUES(\'{}\', \'{}\', 8)")
+        cursor.execute(better.format(keyword, seriesLink))
+        con.commit()
 
-intro = str(input("Welcome to Comicopy!! What would you like to do? \n \nd: download comics\np: post comic title to database\n\n:"))
+
+
+    
+
+intro = str(input("Welcome to Comicopy!! What would you like to do? \n \nd: download comics\np: post comic title to database\nr: remove a comic from library\n\n"))
 
 if intro == "d":
     #comic downloader
     title = input("What is the Keyword of the Comic you'd like to download?") #add a "press h for help" to display all keys
 
 elif intro == "p":
-    #comic uploader
-    seriesLink = input("Please paste the link of the comic series:\n")
-    keyword = input("\nWhat would you like they keyword for this Series to be?\n")
-    fake = {
-        keyword: seriesLink
-    }
-    z = open("db.json").read()
-    open("db.json", 'w').write(str(z).replace("}", ",\n") + json.dumps(fake).replace("{", ""))
+     uploader()
+
+elif intro == "r":
+    end = input("\nPlease enter the key of the comic you would like to remove from your library, enter h to go home, or x to exit this program")
+    if end == "h":
+        home()
+    elif end == "x":
+        None
+    else:
+        cursor.execute("DELETE FROM comic WHERE key = \'" + end + "\'")
+        con.commit()
+        error("\nSuccesfully deleted " + end + ", if it even existed in the first place..\nr/h: home\nx: exit program ")
+
+
 
     
     
 
 
-
+con.commit()
+con.close() 
--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import sqlite3
-con = sqlite3.connect("demo.db")
+con = sqlite3.connect("library.db")
 cur = con.cursor()
 # cur.execute("CREATE TABLE comic(key, link, last page)")
 # cur.execute("""
 #             INSERT INTO comic VALUES
 #             ('USM', 'ultimate spiderman.com', 8),('Iron Man', 'im.com', 10)""")
 # con.commit()
-# res = cur.execute("SELECT link FROM comic WHERE key = 'USM'")
+# res = cur.execute("SELECT link FROM comic WHERE key = 'k'")
 
-cur.execute("DELETE FROM comic WHERE key = 'Iron Man'")
-
+cur.execute("DELETE FROM comic WHERE key = 'k'")
+# keyword ="key"
+# print(cur.execute("SELECT * FROM comic WHERE key = \'" + keyword + "\'").fetchall())
 
 # print(str(res.fetchone()).replace('(','').replace(')', '').replace(',', '').replace('\'', '').replace(',', ''))
 
